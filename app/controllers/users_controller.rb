d4e60010385e257a2edfc316c304e53b20614833
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-05 13:58:52 -0600

    Feat: US2: user registration page
    Co-authored-by: Michael Callahan <calforcal@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    new_user = User.new(user_params)
+    if new_user.save
+      redirect_to user_path(new_user)
+    else
+      redirect_to register_path
+      flash[:alert] = "Error: #{error_message(new_user.errors)}"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@
 class User < ApplicationRecord
   has_many :user_parties, dependent: :restrict_with_error
   has_many :viewing_parties, through: :user_parties
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
