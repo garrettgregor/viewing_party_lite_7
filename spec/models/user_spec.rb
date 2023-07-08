284b7b5c4ac9bcfa75fc235b89f86ed42f21d9d2
Michael Callahan <mcalla242@gmail.com>
2023-07-08 15:57:59 -0600

    Add model method specs for user and viewing party
    Co-authored-by: Garrett Gregor <118634754+garrettgregor@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,44 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
   end
+
+  describe 'instance methods' do
+    let!(:user1) { User.create!(name: 'Michael', email: 'mcalla123@gmail.com') }
+    let!(:user2) { User.create!(name: 'Garrett', email: 'garrett123@gmail.com') }
+    let!(:user3) { User.create!(name: 'Shannon', email: 'shannon123@gmail.com') }
+
+    let!(:viewing_party_1) { ViewingParty.create!(
+      duration: 175,
+      start_date: 'Wed, 07 Aug 2024',
+      start_time: 'Sat, 01 Jan 2000 19:00:00.000000000 UTC +00:00',
+      movie_id: 238,
+      host_id: user1.id
+    ) }
+
+    let!(:viewing_party_2) { 
+      ViewingParty.create!(
+        duration: 122,
+        start_date: 'Wed, 08 Aug 2024',
+        start_time: 'Sat, 01 Jan 2000 19:00:00.000000000 UTC +00:00',
+        movie_id: 239,
+        host_id: user1.id
+      )
+    }
+    
+    let!(:viewing_party_3) { 
+      ViewingParty.create!(
+        duration: 202,
+        start_date: 'Wed, 09 Aug 2024',
+        start_time: 'Sat, 01 Jan 2000 19:00:00.000000000 UTC +00:00',
+        movie_id: 240,
+        host_id: user2.id
+      )
+    }
+
+    it 'returns a list of the viewing parties a user is hosting', :vcr do
+      expect(user1.hosted_parties).to eq([viewing_party_1, viewing_party_2])
+      expect(user2.hosted_parties).to eq([viewing_party_3])
+      expect(user3.hosted_parties).to eq([])
+    end
+  end
 end
--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -7,4 +7,44 @@ RSpec.describe ViewingParty, type: :model do
     it { should have_many(:user_parties) }
     it { should have_many(:users).through(:user_parties) }
   end
+
+  describe 'instance methods' do
+    let!(:user1) { User.create!(name: 'Michael', email: 'mcalla123@gmail.com') }
+    let!(:user2) { User.create!(name: 'Garrett', email: 'garrett123@gmail.com') }
+    let!(:user3) { User.create!(name: 'Shannon', email: 'shannon123@gmail.com') }
+
+    let!(:viewing_party_1) { ViewingParty.create!(
+      duration: 175,
+      start_date: 'Wed, 07 Aug 2024',
+      start_time: 'Sat, 01 Jan 2000 19:00:00.000000000 UTC +00:00',
+      movie_id: 238,
+      host_id: user1.id
+    ) }
+
+    let!(:viewing_party_2) { 
+      ViewingParty.create!(
+        duration: 122,
+        start_date: 'Wed, 08 Aug 2024',
+        start_time: 'Sat, 01 Jan 2000 19:00:00.000000000 UTC +00:00',
+        movie_id: 239,
+        host_id: user1.id
+      )
+    }
+    
+    let!(:viewing_party_3) { 
+      ViewingParty.create!(
+        duration: 202,
+        start_date: 'Wed, 09 Aug 2024',
+        start_time: 'Sat, 01 Jan 2000 19:00:00.000000000 UTC +00:00',
+        movie_id: 240,
+        host_id: user2.id
+      )
+    }
+
+    it 'returns a list of the viewing parties a user is hosting', :vcr do
+      expect(viewing_party_1.host_name).to eq('Michael')
+      expect(viewing_party_2.host_name).to eq('Michael')
+      expect(viewing_party_3.host_name).to eq('Garrett')
+    end
+  end
 end
