9b52879d6b0d00f2630799a874814f7ed2a321fa
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-18 17:13:12 -0600

    feat: add welcome message for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     new_user = User.new(user_params)
     if new_user.save
       session[:user_id] = new_user.id
+      flash[:success] = "Welcome, #{new_user.name}!"
       redirect_to user_path(new_user)
     else
       redirect_to register_path
